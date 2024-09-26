ab449ce44858fc85541f8c5b8db27748f85fa8dd
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-09-26 17:52:18 -0300

    Colocr o nome do amigo no campo de amigos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
+let friendList = [];
+
 function add (){
     let friend = document.getElementById('nome-amigo').value;
 
     alert(friend);
+
+    friendList.push(friend);
+
+    alert(friendList);
+
+    document.getElementById('lista-amigos').innerHTML = friendList.join(',');
 }
