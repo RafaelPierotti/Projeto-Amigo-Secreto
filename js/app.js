6051aa80889ab140574af2bf89b592cbf227d50b
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-10-18 13:17:12 -0300

    Minimo de Participantes

--- js/app.js
+++ js/app.js
@@ -6,13 +6,12 @@ function add (){
     if (friend.value == ''){
         alert ('Insira algum nome');
     } else {
-        
     if (friendList.includes(friend.value)){
         alert('Amigo com esse nome já incluído, coloque o sobrenome dele');
     } else {
         friendList.push(friend.value);
     }
-    
+
     document.getElementById('lista-amigos').innerHTML = friendList.join(',');
 
     friend.value = '';
@@ -24,12 +23,16 @@ function random(){
 
     let raffle = document.getElementById('lista-sorteio');
 
-    for (let i = 0; i < friendList.length; i++){
+    if (friendList.length < 3){
+        alert ('O mínimo é de 3 participantes!');
+    } else {
+        for (let i = 0; i < friendList.length; i++){
 
-        if (i == friendList.length - 1){
-            raffle.innerHTML = raffle.innerHTML + friendList[i] + ' --> ' + friendList[0] + '<br>';
-        } else {
-            raffle.innerHTML = raffle.innerHTML + friendList[i] + ' --> ' + friendList[i + 1] + '<br>';
+            if (i == friendList.length - 1){
+                raffle.innerHTML = raffle.innerHTML + friendList[i] + ' --> ' + friendList[0] + '<br>';
+            } else {
+                raffle.innerHTML = raffle.innerHTML + friendList[i] + ' --> ' + friendList[i + 1] + '<br>';
+            }
         }
     }
 }
@@ -47,5 +50,3 @@ function restart (){
     document.getElementById('lista-amigos').innerHTML = '';
     document.getElementById('lista-sorteio').innerHTML = '';
 }
-
-
